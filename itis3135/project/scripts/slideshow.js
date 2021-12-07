f8e2fe5824868c76fde09b1598058c680535bb24
cduke5 <89805731+cduke5@users.noreply.github.com>
2021-12-07 00:38:42 -0500

    Activity 16 and project update

---
+++ itis3135/project/scripts/slideshow.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        slideWidth:1200,
+        pager:false,
+        auto:true,
+        stopAutoOnClick:true,
+        pause:5000,
+        speed:3000
+    });
+  });
